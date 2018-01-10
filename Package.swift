beeb13122150b830b4f416f69d606fa1d485374e
= <renan.rsg@hotmail.com>
2018-01-10 17:52:13 -0200

    FirstCommit (project structure)

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // Date: 2017-09-20 19:30:47 +0000
 import PackageDescription
 let package = Package(
-	name: "PerfectTemplate",
+	name: "GoodMorning",
 	targets: [],
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
