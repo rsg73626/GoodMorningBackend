f987aad3fd65a8690422635aec1f6b20bfb82ba2
= <renan.rsg@hotmail.com>
2018-01-14 17:07:51 -0200

    User model adjust.

--- Sources/GoodMorning/DB/Models/User.swift
+++ Sources/GoodMorning/DB/Models/User.swift
@@ -12,6 +12,18 @@ class User: PostgresStORM, Codable{
     var about: String?
     var contacts: [Contact]?
     
+    //MARK: Types
+    enum CodingKeys: String, CodingKey {
+        case Id = "id"
+        case Name = "name"
+        case Email = "email"
+        case Password = "password"
+        case Photo = "photo"
+        case About = "about"
+        case Contacts = "contacts"
+    }
+    
+    
     //MARK: Initializers
     override init(){
         self.name = ""
@@ -36,6 +48,29 @@ class User: PostgresStORM, Codable{
         self.about = about
         self.contacts = contacts
     }
+    
+    //MARK: Codable
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        do { self.id = try values.decode(Int.self, forKey: .Id) } catch { self.id = nil }
+        self.name = try values.decode(String.self, forKey: .Name)
+        self.email = try values.decode(String.self, forKey: .Email)
+        do { self.password = try values.decode(String.self, forKey: .Password) } catch { self.password = nil }
+        do { self.photo = try values.decode(String.self, forKey: .Photo) } catch { self.photo = nil }
+        do { self.about = try values.decode(String.self, forKey: .About) } catch { self.about = nil }
+        do { self.contacts = try values.decode([Contact].self, forKey: .Contacts) } catch { self.contacts = [Contact]() }
+        
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.id, forKey: .Id)
+        try container.encode(self.name, forKey: .Name)
+        try container.encode(self.email, forKey: .Email)
+        try container.encode(self.photo, forKey: .Photo)
+        try container.encode(self.about, forKey: .About)
+        try container.encode(self.contacts, forKey: .Contacts)
+    }
 
     
     //MARK: PostgresStORM
@@ -43,8 +78,8 @@ class User: PostgresStORM, Codable{
     
     override func to(_ this: StORMRow) {
         id = this.data["id"] as? Int ?? 0
-        name = this.data["name"] as? String ?? ""
-        email = this.data["email"] as? String ?? ""
+        name = this.data["name"] as! String
+        email = this.data["email"] as! String
         photo = this.data["photo"] as? String ?? ""
         about = this.data["about"] as? String ?? ""
         contacts = ContactQuery.readByUserId(id!)
