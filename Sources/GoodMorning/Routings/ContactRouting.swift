454e454fdaa765b6b1d35387a67020980f6bf7c2
= <renan.rsg@hotmail.com>
2018-01-14 17:11:14 -0200

    Contact routing adjusts.

--- Sources/GoodMorning/Routings/ContactRouting.swift
+++ Sources/GoodMorning/Routings/ContactRouting.swift
@@ -9,7 +9,7 @@ class ContactRouting{
         routes.add(method: .post, uri: "/", handler: ContactHandler.create)
         routes.add(method: .get, uri: "/", handler: ContactHandler.read)
         routes.add(method: .get, uri: "/{id}/", handler: ContactHandler.readById)
-        routes.add(method: .put, uri: "/{id}/", handler: ContactHandler.update)
+        routes.add(method: .put, uri: "/", handler: ContactHandler.update)
         routes.add(method: .delete, uri: "/{id}/", handler: ContactHandler.delete)
         
         return routes
