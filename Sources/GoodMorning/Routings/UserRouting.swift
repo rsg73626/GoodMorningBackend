e0c3aac1db0478201d13f83da7aba1c9a8bb8059
= <renan.rsg@hotmail.com>
2018-01-14 17:10:57 -0200

    User routing adjusts.

--- Sources/GoodMorning/Routings/UserRouting.swift
+++ Sources/GoodMorning/Routings/UserRouting.swift
@@ -7,6 +7,7 @@ class UserRouting{
         var routes: Routes = Routes()
         
         routes.add(method: .post, uri: "/", handler: UserHandler.create)
+        routes.add(method: .post, uri: "/login/", handler: UserHandler.login)
         routes.add(method: .get, uri: "/", handler: UserHandler.read)
         routes.add(method: .get, uri: "/{id}/", handler: UserHandler.readById)
         routes.add(method: .put, uri: "/", handler: UserHandler.update)
